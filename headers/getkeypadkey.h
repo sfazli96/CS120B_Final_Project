8a8d1a3dfe8c5f79ba73ba491741323bbb966401
Sameh <sfazli96@gmail.com>
2020-03-10 13:14:35 -0700

    Added header files

---
+++ headers/getkeypadkey.h
@@ -0,0 +1,53 @@
+// Returns '\0' if no key pressed, else returns char '1', '2', ... '9', 'A', ...
+// If multiple keys pressed, returns leftmost-topmost one
+// Keypad must be connected to port C
+/* Keypad arrangement
+        PC4 PC5 PC6 PC7
+   col  1   2   3   4
+row
+PC0 1   1 | 2 | 3 | A
+PC1 2   4 | 5 | 6 | B
+PC2 3   7 | 8 | 9 | C
+PC3 4   * | 0 | # | D
+*/
+#include "bit.h"
+unsigned char GetKeypadKey() {
+	PORTC = 0xEF; // Enable col 4 with 0, disable others with 1’s
+	asm("nop"); // add a delay to allow PORTC to stabilize before checking
+	if (GetBit(PINC,0)==0) { return('1'); }
+	if (GetBit(PINC,1)==0) { return('4'); }
+	if (GetBit(PINC,2)==0) { return('7'); }
+	if (GetBit(PINC,3)==0) { return('*'); }
+
+	// Check keys in col 2
+	PORTC = 0xDF; // Enable col 5 with 0, disable others with 1’s
+	asm("nop"); // add a delay to allow PORTC to stabilize before checking
+	if (GetBit(PINC,0)==0) { return('2'); }
+	if (GetBit(PINC,1)==0) { return('5'); }
+	if (GetBit(PINC,2)==0) { return('8'); }
+	if (GetBit(PINC,3)==0) { return('0'); }
+
+	// ... *****FINISH*****
+
+	// Check keys in col 3
+	PORTC = 0xBF; // Enable col 6 with 0, disable others with 1’s
+	asm("nop"); // add a delay to allow PORTC to stabilize before checking
+	if (GetBit(PINC,0)==0) { return('3'); }
+	if (GetBit(PINC,1)==0) { return('6'); }
+	if (GetBit(PINC,2)==0) { return('9'); }
+	if (GetBit(PINC,3)==0) { return('#'); }
+
+	// ... *****FINISH*****
+
+	// Check keys in col 4	
+	PORTC = 0x7F; // Enable col 6 with 0, disable others with 1’s
+	asm("nop"); // add a delay to allow PORTC to stabilize before checking
+	if (GetBit(PINC,0)==0) { return('A'); }
+	if (GetBit(PINC,1)==0) { return('B'); }
+	if (GetBit(PINC,2)==0) { return('C'); }
+	if (GetBit(PINC,3)==0) { return('D'); }
+	// ... *****FINISH*****
+	
+
+	return('\0'); // default value
+	}
