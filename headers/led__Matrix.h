f1b3f2d97a2ba6b796c6efc60db4d161dbdb1ec5
sfazli96 <43709736+sfazli96@users.noreply.github.com>
2020-03-11 23:09:55 -0700

    Updated LED matrix

--- headers/led__Matrix.h
+++ headers/led__Matrix.h
@@ -5,6 +5,7 @@
  * Author : sfazl001 Sameh Fazli 
  */ 
 
+#include "Global.h"
 #ifndef _LED_Matrix_H_
 #define _LED_Matrix_H_
 #include <stdio.h>
@@ -26,39 +27,56 @@ int LED_Matrix(int state)
 	switch(state)
 	{
 		case display:
-		if(STATE == 0)
+		if(GameOn == 1)
 		{
-			shift2Write(0xFF);
-			shiftWrite(0x00);
-			STATE = 1;
-			++i;
-		}
-		else
-		{
-			shift2Write(0xFF & (~(l_list[j][i].row)) );
-			shiftWrite(0x00 | (l_list[j][i].column));
-			STATE = 0;
-		}
-		if(i >= 8)
-		{
-			i = 0;
-		}
-		if(k < 500)
-		{
-			++k;
-		}
-		else
-		{
-			k=0;
-			if(j < 10)
-			{
-				//j++;
-				j = Get_Num();
+			if(STATE == 0)   {
+				shift2Write(0xFF);
+				shiftWrite(0x00);
+				if(counter == 0)
+				{
+					Rand_Num = Get_Num();
+					Rand_Num2 = Get_Num();
+				}
+				STATE = 1;
+				++i;
 			}
 			else
 			{
-				j = 0;
+				shift2Write(0xFF & (~(l_list[j][i].row)) );
+				shiftWrite(0x00 | (l_list[j][i].column));
+				STATE = 0;
 			}
+			if(i >= 8)
+			{
+				i = 0;
+			}
+			if(k < 500)
+			{
+				++k;
+			}
+			else
+			{
+				k=0;
+				if(j < 10)
+				{
+					//j++;
+					j = Get_Num(); // Generate random character 1 random character a specific time.
+					Rand_Num = j;
+					if(counter == 0)
+					{
+						j = Rand_Num;
+					}
+					else
+					{
+						j = Rand_Num2;
+					}
+				}
+			}
+		}
+		else
+		{
+			shift2Write(0xFF);
+			shiftWrite(0x00);
 		}
 		break;
 	}
