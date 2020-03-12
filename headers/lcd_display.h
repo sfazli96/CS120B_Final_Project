e1b6d5a3275c6a206b171140869fa49371389b59
sfazli96 <43709736+sfazli96@users.noreply.github.com>
2020-03-11 23:11:33 -0700

    Display Win and Loss

--- headers/lcd_display.h
+++ headers/lcd_display.h
@@ -1,27 +1,60 @@
 /*
- * LCD_Display.h
+ * LCD_Display.c
  *
  * Created: 3/10/2020 1:35:37 AM
  * Author : sfazl001 Sameh Fazli
  */ 
+#include "Global.h"
 #ifndef _LCD_Display_H_
 #define _LCD_Display_H_
-enum states{LCDdisplay};
+enum states{MENU, Game};
 int LCD_Display(int state)
 {
 	switch(state)
 	{
-		case LCDdisplay:
-		state = LCDdisplay;
+		case MENU:
+		if(GameOn == 1)
+		{
+			state = Game;
+			LCD_ClearScreen();
+		}
+		else
+		{
+			GameOn = 0;
+			state = MENU;
+		}
 		break;	
+		case Game:
+		if(GameOn == 0)
+		{
+			state = MENU;
+			LCD_ClearScreen();
+		}
+		else
+		{
+			GameOn = 1;
+			state = Game;
+		}
 	}
 	
 	// state actions 
 	switch(state)
 	{
-		case LCDdisplay:
+		case MENU:
+		LCD_Cursor(1);
+		LCD_DisplayString(1, "Press start to play" );
+		break;
+		case Game:
 		LCD_Cursor(1);
 		LCD_DisplayString(1, "Score: ");
+		if(score == 5)
+		{
+			LCD_DisplayString(1, "You win! Press A to play again!");
+		}
+		else if(lives == 0 || score != 5)
+		{
+			LCD_DisplayString(1, "You Lose! Press A to play again");
+		}
 		break;
 	}
 	return state;
