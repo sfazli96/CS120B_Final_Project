3f5c7981e0a8b516dd862d25028dcda0115594ad
sfazli96 <43709736+sfazli96@users.noreply.github.com>
2020-03-12 13:47:32 -0700

    Removed random Generator

--- headers/led__Matrix.h
+++ headers/led__Matrix.h
@@ -3,6 +3,7 @@
  *
  * Created: 3/10/2020 1:21:36 PM
  * Author : sfazl001 Sameh Fazli 
+ * Cs120b
  */ 
 
 #include "Global.h"
@@ -11,7 +12,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 static uint8_t i = 0;
-static uint8_t j = 0;
 static uint16_t k = 0;
 static uint8_t STATE = 0;
 enum LEDMatrix{display};
@@ -20,57 +20,62 @@ int Get_Num() {
 	int num;
 	num = (rand() % 11);
 	return num;
-}// Get_Num  
+}// Get_Num, random generator
 
 int LED_Matrix(int state)
 {
 	switch(state)
 	{
-		case display:
+		case display:   // This big If and else statement, whole thing is able to display the LED Matrix.
 		if(GameOn == 1)
 		{
 			if(STATE == 0)   {
 				shift2Write(0xFF);
 				shiftWrite(0x00);
-				if(counter == 0)
+				/*if(counter == 0)
 				{
-					Rand_Num = Get_Num();
-					Rand_Num2 = Get_Num();
+					Rand_Char = Get_Num();   // get Random Character
+					Rand_Char2 = Get_Num();  // get Random Character
 				}
+				*/
 				STATE = 1;
 				++i;
 			}
 			else
 			{
-				shift2Write(0xFF & (~(l_list[j][i].row)) );
-				shiftWrite(0x00 | (l_list[j][i].column));
+				if(matrixControl)   {
+					shift2Write(0xFF & (~(l_list[j][i].row)) );
+					shiftWrite(0x00 | (l_list[j][i].column));
+				}
 				STATE = 0;
 			}
-			if(i >= 8)
+			if(i >= 8) // Iterates through all the lines
 			{
 				i = 0;
 			}
-			if(k < 500)
+			if(k < 500)  // Timer 
 			{
 				++k;
 			}
 			else
 			{
 				k=0;
-				if(j < 10)
-				{
-					//j++;
-					j = Get_Num(); // Generate random character 1 random character a specific time.
-					Rand_Num = j;
-					if(counter == 0)
+				//if(j < 10)   // This is how many byte array of the LED Matrix that I have and which byte arrays that I want to print out
+				//{
+					/*if(counter == 0)
 					{
-						j = Rand_Num;
+						j = Rand_Char;
 					}
-					else
+					else if(counter == 1)
 					{
-						j = Rand_Num2;
+						j = Rand_Char2;
 					}
-				}
+					*/
+				//}
+				//else
+				//{
+					//j = 0;	
+				//}
 			}
 		}
 		else
